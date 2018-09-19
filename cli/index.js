954415b43861ef18670a8fa666040f739b51df9c
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-09-19 17:41:08 -0400

    Declare main, display and prompt methods

--- cli/index.js
+++ cli/index.js
@@ -6,6 +6,24 @@ const cTable = require("console.table");
 
 // Declare global variables
 let txnArr = []; // Array to store queried transactions locally
+const reportTypes = [
+    {
+        id: 1,
+        name: "Sales Summary"
+    },
+    {
+        id: 2,
+        name: "Assets Under Management Summary"
+    },
+    {
+        id: 3,
+        name: "Break Report"
+    },
+    {
+        id: 4,
+        name: "Investor Profit"
+    }
+];
 
 // MySQL DB Connection Information
 const connection = mysql.createConnection({
@@ -22,4 +40,53 @@ connection.connect(function(err) {
         return;
     }
     console.log("connected as id " + connection.threadId);
+    app.main();
 });
+
+// Declare functions
+const app = {
+    main: function() {
+        // Goto display
+        this.display(this.prompt);
+    },
+    // Display table of all transactions
+    display: function(callback) {
+        const query = "SELECT * FROM invest";
+        connection.query(query, function(err, result) {
+            if (err) {
+                console.error("query error: " + err);
+            }
+            // Clear the txnArr for each new query
+            txnArr = [];
+            for (var i = 0; i < result.length; i++) {
+                txnArr.push(result[i]);
+            }
+            console.table(reportTypes);
+        });
+        // Goto prompt via callback
+        setTimeout(callback, 500);
+    },
+    // Prompt user for input
+    prompt: function() {
+        inquirer.prompt([
+            {
+                name: "id",
+                type: "input",
+                message: "Please enter the Report ID you would like to view",
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                        // Check that report ID is valid
+                        for (let i = 0; i < reportTypes.length; i++) {
+                            if (reportTypes[i].id === parseInt(value)) {
+                                return true;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        ]).then(function(answer) {
+            console.log("You chose Report ID: " + answer.id);
+        });
+    }
+};
