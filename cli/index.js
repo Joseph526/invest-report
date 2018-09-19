a778067e28345e0032db9d972e3b76c39342745a
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-09-19 16:48:06 -0400

    Create MySQL connection

--- cli/index.js
+++ cli/index.js
@@ -1,5 +1,25 @@
 // Require dependencies
 require("dotenv").config();
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-var cTable = require("console.table");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+
+// Declare global variables
+let txnArr = []; // Array to store queried transactions locally
+
+// MySQL DB Connection Information
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PW,
+    database: process.env.MYSQL_DB
+});
+
+// Initiate MySQL Connection.
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
