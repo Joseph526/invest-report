508fed5750c34dd24d76cbf872230438a9e0e99a
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-09-23 18:58:34 -0400

    Create test stub file

---
+++ cli/index.test.js
@@ -0,0 +1,8 @@
+const expect = require("chai").expect;
+const app = require("./index");
+
+describe("Invest-Report App", function() {
+    it("should perform basic calculation", function() {
+        expect(1 + 1).to.equal(2);
+    });
+});
