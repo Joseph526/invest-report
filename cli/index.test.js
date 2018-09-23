c31cb1e8d8329d5441cb5cc8cf46761ca959e16a
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-09-23 19:17:08 -0400

    Add a test for report1

--- cli/index.test.js
+++ cli/index.test.js
@@ -2,7 +2,7 @@ const expect = require("chai").expect;
 const app = require("./index");
 
 describe("Invest-Report App", function() {
-    it("should perform basic calculation", function() {
-        expect(1 + 1).to.equal(2);
+    it("report1 should display two sales reps", function() {
+        expect(app.report1().length).to.equal(2);
     });
 });
